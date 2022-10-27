43d561278941eceec8de809f367475bf46e0cc98
SlavaTatanov <slavatatanov@gmail.com>
2022-10-27 21:24:31 +0500

    list for settings

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.screenmanager import Screen
 from calculating import calculating
-import settings
+from kivy.storage.jsonstore import JsonStore
+from kivymd.uix.list import OneLineIconListItem
+from kivymd.uix.dialog import MDDialog
 
 
 class MainWindow(Screen):
-    settings.settings['currency'] = settings.sql_request_settings('currency')[0]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.settings = settings
+        self.dialog = None
 
     def up_peoples(self, x):
         peoples = int(self.peoples_lbl.text)
@@ -21,11 +27,19 @@ class MainWindow(Screen):
         cons = self.consumption.text
         prc = self.price.text
         peoples = self.peoples_lbl.text
-        self.result.text = f'{calculating(km, cons, prc, peoples)} {settings.settings["currency"]}'
+        self.result.text = f'{calculating(km, cons, prc, peoples)} {self.settings["money"]}'
 
     def change_lang(self):
         import random
         self.sett_lang.secondary_text = random.choice(['Тык-тык', 'Ой-ой-ой'])
+
+    def show_simple_dialog(self, inp: dict):
+        it = inp["items"]
+        items = []
+        for item in it:
+            items.append(OneLineIconListItem(text=item))
+        self.dialog = MDDialog(title=inp["title"], type="simple", items=items)
+        self.dialog.open()
 
 
 class WindowManager(ScreenManager):
@@ -33,10 +47,12 @@ class WindowManager(ScreenManager):
 
 
 class FuelApp(MDApp):
+
     def build(self):
         Builder.load_file("fuel.kv")
         return WindowManager()
 
 
 if __name__ == '__main__':
+    settings = JsonStore("settings.json")
     FuelApp().run()
